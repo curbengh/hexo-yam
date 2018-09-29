2dc6c5f48ead2cbb08a0bba1731ccbe13a9d57ef
weyusi <weyusi@users.noreply.github.com>
2018-09-29 15:08:45 +0930

    Don't run gzip when public folder has/does not exist
    Continuation of 71c5878400040b1e89254b383334aefbe8f67215
    This is necessary especially to prevent gzip from running when hexo clean

--- index.js
+++ index.js
@@ -1,50 +1,48 @@
 /* global hexo */
 var assign = require('object-assign');
 
-//module.exports = function (hexo) {
-    if (true === hexo.config.neat_enable) {
-        // HTML minifier
-        hexo.config.neat_html = assign({
-            enable: true,
-            logger: true,
-            exclude: [],
-            ignoreCustomComments: [/^\s*more/],
-            removeComments: true,
-            removeCommentsFromCDATA: true,
-            collapseWhitespace: true,
-            collapseBooleanAttributes: true,
-            removeEmptyAttributes: true,
-            minifyJS: true,
-            minifyCSS: true,
-        }, hexo.config.neat_html);
+if (true === hexo.config.neat_enable) {
+    // HTML minifier
+    hexo.config.neat_html = assign({
+        enable: true,
+        logger: true,
+        exclude: [],
+        ignoreCustomComments: [/^\s*more/],
+        removeComments: true,
+        removeCommentsFromCDATA: true,
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        removeEmptyAttributes: true,
+        minifyJS: true,
+        minifyCSS: true,
+    }, hexo.config.neat_html);
 
-        // Css minifier
-        hexo.config.neat_css = assign({
-            enable: true,
-            logger: true,
-            exclude: ['*.min.css']
-        }, hexo.config.neat_css);
+    // Css minifier
+    hexo.config.neat_css = assign({
+        enable: true,
+        logger: true,
+        exclude: ['*.min.css']
+    }, hexo.config.neat_css);
 
-        // Js minifier
-        hexo.config.neat_js = assign({
-            enable: true,
-            mangle: true,
-            logger: true,
-            output: {},
-            compress: {},
-            exclude: ['*.min.js']
-        }, hexo.config.neat_js);
+    // Js minifier
+    hexo.config.neat_js = assign({
+        enable: true,
+        mangle: true,
+        logger: true,
+        output: {},
+        compress: {},
+        exclude: ['*.min.js']
+    }, hexo.config.neat_js);
 
-        // html, css, js compression
-        hexo.config.gzip = assign({
-            enable: true,
-            logger: true,
-        }, hexo.config.gzip);
+    // html, css, js compression
+    hexo.config.neat_gzip = assign({
+        enable: true,
+        logger: true,
+    }, hexo.config.neat_gzip);
 
-        var filter = require('./lib/filter');
-        hexo.extend.filter.register('after_render:html', filter.logic_html);
-        hexo.extend.filter.register('after_render:css', filter.logic_css);
-        hexo.extend.filter.register('after_render:js', filter.logic_js);
-        hexo.extend.filter.register('after_generate', filter.logic_gzip);
-    }
-//}
+    var filter = require('./lib/filter');
+    hexo.extend.filter.register('after_render:html', filter.logic_html);
+    hexo.extend.filter.register('after_render:css', filter.logic_css);
+    hexo.extend.filter.register('after_render:js', filter.logic_js);
+    hexo.extend.filter.register('before_exit', filter.logic_gzip);
+}
