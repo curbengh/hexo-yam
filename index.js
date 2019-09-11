7e85bd63380cb695d36d4ffc3d484a17455a19f5
curbengh <43627182+curbengh@users.noreply.github.com>
2019-09-11 02:15:19 +0100

    feat: enable plugin by default and update options naming

--- index.js
+++ index.js
@@ -1,70 +1,68 @@
 /* global hexo */
 'use strict'
 
-if (hexo.config.neat_enable === true) {
-  // HTML minifier
-  hexo.config.neat_html = Object.assign({
-    enable: true,
-    logger: false,
-    exclude: [],
-    collapseBooleanAttributes: true,
-    collapseWhitespace: true,
-    // Ignore '<!-- more -->' https://hexo.io/docs/tag-plugins#Post-Excerpt
-    ignoreCustomComments: [/^\s*more/],
-    removeComments: true,
-    removeEmptyAttributes: true,
-    removeScriptTypeAttributes: true,
-    removeStyleLinkTypeAttributes: true,
-    minifyJS: true,
-    minifyCSS: true,
-    globOptions: { basename: true }
-  }, hexo.config.neat_html)
+hexo.config.minify = Object.assign({
+  enable: true
+}, hexo.config.minify)
 
-  // CSS minifier
-  hexo.config.neat_css = Object.assign({
-    enable: true,
-    logger: false,
-    exclude: ['*.min.css'],
-    level: 2,
-    globOptions: { basename: true }
-  }, hexo.config.neat_css)
+hexo.config.minify.html = Object.assign({
+  enable: true,
+  logger: false,
+  exclude: [],
+  collapseBooleanAttributes: true,
+  collapseWhitespace: true,
+  // Ignore '<!-- more -->' https://hexo.io/docs/tag-plugins#Post-Excerpt
+  ignoreCustomComments: [/^\s*more/],
+  removeComments: true,
+  removeEmptyAttributes: true,
+  removeScriptTypeAttributes: true,
+  removeStyleLinkTypeAttributes: true,
+  minifyJS: true,
+  minifyCSS: true,
+  globOptions: { basename: true }
+}, hexo.config.minify.html)
 
-  // Javascript minifier
-  hexo.config.neat_js = Object.assign({
-    enable: true,
-    logger: false,
-    exclude: ['*.min.js'],
-    compress: {},
-    mangle: true,
-    output: {},
-    globOptions: { basename: true }
-  }, hexo.config.neat_js)
+hexo.config.minify.css = Object.assign({
+  enable: true,
+  logger: false,
+  exclude: ['*.min.css'],
+  level: 2,
+  globOptions: { basename: true }
+}, hexo.config.minify.css)
 
-  // SVG minifier
-  hexo.config.neat_svg = Object.assign({
-    enable: true,
-    logger: false,
-    include: ['*.svg', '!*.min.svg'],
-    plugins: [],
-    globOptions: { basename: true }
-  }, hexo.config.neat_svg)
+hexo.config.minify.js = Object.assign({
+  enable: true,
+  logger: false,
+  exclude: ['*.min.js'],
+  compress: {},
+  mangle: true,
+  output: {},
+  globOptions: { basename: true }
+}, hexo.config.minify.js)
 
-  // gzip compression
-  hexo.config.neat_gzip = Object.assign({
-    enable: true,
-    logger: false,
-    include: ['*.html', '*.css', '*.js', '*.txt', '*.ttf', '*.atom', '*.stl', '*.xml', '*.svg', '*.eot', '*.json'],
-    globOptions: { basename: true }
-  }, hexo.config.neat_gzip)
+hexo.config.minify.svg = Object.assign({
+  enable: true,
+  logger: false,
+  include: ['*.svg', '!*.min.svg'],
+  plugins: [],
+  globOptions: { basename: true }
+}, hexo.config.minify.svg)
 
-  // brotli compression
-  hexo.config.neat_brotli = Object.assign({
-    enable: true,
-    logger: false,
-    include: ['*.html', '*.css', '*.js', '*.txt', '*.ttf', '*.atom', '*.stl', '*.xml', '*.svg', '*.eot', '*.json'],
-    globOptions: { basename: true }
-  }, hexo.config.neat_brotli)
+hexo.config.minify.gzip = Object.assign({
+  enable: true,
+  logger: false,
+  include: ['*.html', '*.css', '*.js', '*.txt', '*.ttf', '*.atom', '*.stl', '*.xml', '*.svg', '*.eot', '*.json'],
+  globOptions: { basename: true }
+}, hexo.config.minify.gzip)
 
+hexo.config.minify.brotli = Object.assign({
+  enable: true,
+  logger: false,
+  include: ['*.html', '*.css', '*.js', '*.txt', '*.ttf', '*.atom', '*.stl', '*.xml', '*.svg', '*.eot', '*.json'],
+  globOptions: { basename: true }
+}, hexo.config.minify.brotli)
+
+if (hexo.config.minify.enable === true) {
   const filter = require('./lib/filter')
   hexo.extend.filter.register('after_render:html', filter.logicHtml)
   hexo.extend.filter.register('after_render:css', filter.logicCss)
